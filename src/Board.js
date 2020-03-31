2e114323d4b302e9028c9dfd9598589ff7b4fc57
Ananda Rodrigues! <ananda.mohini.dasi@hotmail.com>
2020-03-31 11:25:09 +0300

    Initial setup

--- src/Board.js
+++ src/Board.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './Board.css';
 
-function App() {
+function Board() {
   return (
     <div className="App">
       <header className="App-header">
@@ -23,4 +23,4 @@ function App() {
   );
 }
 
-export default App;
+export default Board;
